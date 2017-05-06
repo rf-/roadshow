c04b457dfa23ac727c8709779b41bba1b4ae8b1b
Ryan Fitzgerald <rwfitzge@gmail.com>
2017-05-06 15:37:33 -0700

    Allow cleaning up only images or only volumes
    This is useful for cases where, e.g., you need to rebuild your images
    but don't want to lose volumes containing installed gems.

--- spec/commands/clean_spec.cr
+++ spec/commands/clean_spec.cr
@@ -51,4 +51,70 @@ describe Roadshow::Commands::Clean do
       end
     end
   end
+
+  it "cleans up only images" do
+    SpecHelper.in_project("ruby") do
+      begin
+        SpecHelper.run!(["run"])
+
+        images = `docker images`
+        images.should contain("ruby_scenario_one")
+        images.should contain("ruby_scenario_two")
+
+        volumes = `docker volume ls`
+        volumes.should contain("ruby_bundle_one")
+        volumes.should contain("ruby_bundle_two")
+
+        output = SpecHelper.run!(["clean", "-i"])
+        output.should contain("Untagged: ruby_scenario_one:latest")
+        output.should contain("Untagged: ruby_scenario_two:latest")
+        output.should_not contain("ruby_bundle_one")
+        output.should_not contain("ruby_bundle_two")
+
+        images = `docker images`
+        images.should_not contain("ruby_scenario_one")
+        images.should_not contain("ruby_scenario_two")
+
+        volumes = `docker volume ls`
+        volumes.should contain("ruby_bundle_one")
+        volumes.should contain("ruby_bundle_two")
+      ensure
+        FileUtils.rm("scenarios/one.gemfile.lock")
+        FileUtils.rm("scenarios/two.gemfile.lock")
+      end
+    end
+  end
+
+  it "cleans up only volumes" do
+    SpecHelper.in_project("ruby") do
+      begin
+        SpecHelper.run!(["run"])
+
+        images = `docker images`
+        images.should contain("ruby_scenario_one")
+        images.should contain("ruby_scenario_two")
+
+        volumes = `docker volume ls`
+        volumes.should contain("ruby_bundle_one")
+        volumes.should contain("ruby_bundle_two")
+
+        output = SpecHelper.run!(["clean", "-v"])
+        output.should_not contain("Untagged: ruby_scenario_one:latest")
+        output.should_not contain("Untagged: ruby_scenario_two:latest")
+        output.should contain("ruby_bundle_one")
+        output.should contain("ruby_bundle_two")
+
+        images = `docker images`
+        images.should contain("ruby_scenario_one")
+        images.should contain("ruby_scenario_two")
+
+        volumes = `docker volume ls`
+        volumes.should_not contain("ruby_bundle_one")
+        volumes.should_not contain("ruby_bundle_two")
+      ensure
+        FileUtils.rm("scenarios/one.gemfile.lock")
+        FileUtils.rm("scenarios/two.gemfile.lock")
+      end
+    end
+  end
 end
--- src/roadshow/commands/clean.cr
+++ src/roadshow/commands/clean.cr
@@ -1,17 +1,32 @@
 module Roadshow
   module Commands
     class CleanOptions
+      property :images
+      @images : Bool = true
+
+      property :volumes
+      @volumes : Bool = true
     end
 
     class Clean < Command(CleanOptions)
       def parser(stdout : IO, options = CleanOptions.new)
         OptionParser.new do |parser|
           parser.banner = <<-BANNER
-          Usage: roadshow clean
+          Usage: roadshow clean [options]
 
           Based on your project's `#{SCENARIOS_FILENAME}`, remove all Docker
           images created to run your project's scenarios.
           BANNER
+
+          parser.separator
+
+          parser.on("-i", "--images-only", "Only remove images") do
+            options.volumes = false
+          end
+
+          parser.on("-v", "--volumes-only", "Only remove volumes") do
+            options.images = false
+          end
         end
       end
 
@@ -24,50 +39,54 @@ module Roadshow
 
         config = ProjectConfig.load(YAML.parse(File.read(SCENARIOS_FILENAME)))
 
-        images_io = IO::Memory.new
+        if options.images
+          images_io = IO::Memory.new
 
-        Process.run(
-          "docker",
-          ["images"],
-          input: stdin,
-          output: images_io,
-          error: stdout
-        ).success? || raise CommandFailed.new
-
-        images = images_io.to_s.lines.skip(1).map { |l| l.split[0] }
-        images_to_delete = images & config.scenarios.map(&.image_name)
-
-        if images_to_delete.any?
           Process.run(
             "docker",
-            ["rmi"] + images_to_delete,
+            ["images"],
             input: stdin,
-            output: stdout,
+            output: images_io,
             error: stdout
           ).success? || raise CommandFailed.new
+
+          images = images_io.to_s.lines.skip(1).map { |l| l.split[0] }
+          images_to_delete = images & config.scenarios.map(&.image_name)
+
+          if images_to_delete.any?
+            Process.run(
+              "docker",
+              ["rmi"] + images_to_delete,
+              input: stdin,
+              output: stdout,
+              error: stdout
+            ).success? || raise CommandFailed.new
+          end
         end
 
-        volumes_io = IO::Memory.new
+        if options.volumes
+          volumes_io = IO::Memory.new
 
-        Process.run(
-          "docker",
-          ["volume", "ls"],
-          input: stdin,
-          output: volumes_io,
-          error: stdout
-        ).success? || raise CommandFailed.new
-
-        volumes = volumes_io.to_s.lines.skip(1).map { |l| l.split[1] }
-        volumes_to_delete = volumes & config.scenarios.flat_map(&.volume_names)
-
-        if volumes_to_delete.any?
           Process.run(
             "docker",
-            ["volume", "rm"] + volumes_to_delete,
+            ["volume", "ls"],
             input: stdin,
-            output: stdout,
+            output: volumes_io,
             error: stdout
           ).success? || raise CommandFailed.new
+
+          volumes = volumes_io.to_s.lines.skip(1).map { |l| l.split[1] }
+          volumes_to_delete = volumes & config.scenarios.flat_map(&.volume_names)
+
+          if volumes_to_delete.any?
+            Process.run(
+              "docker",
+              ["volume", "rm"] + volumes_to_delete,
+              input: stdin,
+              output: stdout,
+              error: stdout
+            ).success? || raise CommandFailed.new
+          end
         end
       rescue e : InvalidConfig
         stdout.puts "Error: #{e.message}".colorize(:red)
